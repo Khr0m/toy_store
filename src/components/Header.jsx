a58423a1c40355db86f31c5fedd7844ff3bb5e07
Khr0m <154710366+Khr0m@users.noreply.github.com>
2024-12-26 22:47:14 +0500

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header.css';
+import Logo from '../images/header/ButterflyBlue.png';
+import telegram from '../images/header/IconTelegram.png';
+import vk from '../images/header/IconVK.png';
+import almostForbidden from '../images/header/IconWhatsapp.png';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <img src={Logo} alt="Логотип магазина" className="logo" />
+        <div className="title">
+          <h1>Сказочный мир</h1>
+          <p className="slogan">"Подарите детям волшебство и игры"</p>
+        </div>
+        <div className="contact-info">
+          <p className="phone">+7 1234567890</p>
+          <p className="free-call">Звонок бесплатный</p>
+          <div className="social-icons">
+            <a href="https://telegram.org" target="_blank" rel="noopener noreferrer">
+              <img src={telegram} alt="Telegram" />
+            </a>
+            <a href="https://vk.com" target="_blank" rel="noopener noreferrer">
+              <img src={vk} alt="ВКонтакте" />
+            </a>
+            <a href="https://whatsapp.com" target="_blank" rel="noopener noreferrer">
+              <img src={almostForbidden} alt="WhatsApp" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './Header';
 
 const Navbar = ({ cart }) => {
   return (
+    <>
+    <Header /> {}
     <nav>
       <Link to="/">Главная</Link>
       <Link to="/products">Каталог</Link>
       <Link to="/contact">Контакты</Link>
       <Link to="/cart">Корзина ({cart.length})</Link>
     </nav>
+    </>
   );
 };
 
